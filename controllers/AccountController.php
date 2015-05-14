46e4cc0c7458ad25cc660e986abebdd241316e6c
aafetsrom <aafetsrom@HTG-LP-59.hta.local>
2015-05-14 11:39:28 -0300

    html finishes

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -131,7 +131,7 @@ class AccountController extends Controller
         if (($model = Account::findOne($id)) !== null) {
             return $model;
         } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
+           // throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
 }
