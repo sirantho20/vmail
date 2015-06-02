c4a528d6cd6023361f632f03af40581899d16780
aafetsrom <aafetsrom@HTG-LP-59.hta.local>
2015-06-02 14:22:02 -0300

    transaction controller and UI fixes

--- migrations/m150215_162212_dbUpdate.php
+++ migrations/m150215_162212_dbUpdate.php
@@ -8,14 +8,14 @@ class m150215_162212_dbUpdate extends Migration
     
     public function safeup()
     {
-        $this->execute(file_get_contents(\Yii::getAlias('@app').'/data/db.sql'));
+        //$this->execute(file_get_contents(\Yii::getAlias('@app').'/data/db.sql'));
     }
 
     public function safedown()
     {
-        $this->dropTable('account_users');
-        $this->dropTable('account_subscription');
-        $this->dropTable('account');
-        $this->dropTable('account_package');
+//        $this->dropTable('account_users');
+//        $this->dropTable('account_subscription');
+//        $this->dropTable('account');
+//        $this->dropTable('account_package');
     }
 }
