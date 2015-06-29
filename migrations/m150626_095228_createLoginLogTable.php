4e90b007d19e677e549a3a65308d3a6015284dd6
aafetsrom <aafetsrom@HTG-LP-59.hta.local>
2015-06-29 13:22:38 -0300

    logging table added

---
+++ migrations/m150626_095228_createLoginLogTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150626_095228_createLoginLogTable extends Migration
+{
+    public function up()
+    {
+        $this->createTable('login_log', [
+            'id' => Schema::TYPE_PK,
+            'username' => Schema::TYPE_STRING,
+            'login_date' => Schema::TYPE_DATETIME,
+            'login_ip' => Schema::TYPE_STRING,
+        ]);
+        
+        
+    }
+
+    public function down()
+    {
+        $this->dropTable('login_log');
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
